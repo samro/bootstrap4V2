f6dc7919a06044c73472bc4931dd5b94a155ac4a
Samuel <samuel.rossi@villedupre.fr>
2018-12-18 12:05:33 +0100

    change css

--- test.js
+++ test.js
@@ -37,16 +37,6 @@ function animation_menus_principaux() {
                 $("main").css("left", "200px");
             }
         }
-
-        //test pour recuperer la valeur et attribuer  la class de navbar gauche au moment du click 
-        $("#test_classe_gauche").val($("#navbarleft").attr("class"));
-        //test compteur sur le boutton gauche au moment du click
-        //$("#test_bouton_gauche").val(countleft);
-        //test pour recuperer la valeur de la variable(action_utilisateur_gauche)
-        //$("#test_variable_gauche").val(action_utilisateur_gauche);
-        $("#test_left").val($("main").css("left") + "nb de clic" +countmenuicon + "taille_menu" + taille_menu_gauche);
-       
-
     });
     
     
@@ -73,6 +63,7 @@ function animation_menus_principaux() {
           $("main").css("right", "0px");
       } else {
           $("#navbarright").css("right", "0px");
+          $("main").css("right", "200px");
       }
     });
 
@@ -85,40 +76,18 @@ function animation_menus_principaux() {
             $("main").css("left", "50px");
             $(".text_menu").css("display", "none");
             taille_menu_gauche = "petit";
-            //  alert("50px");
         } else {
             $("#navbarleft").css("width", "200px");
             $("main").css("left", "200px");
             $(".text_menu").css("display", "inline");
             taille_menu_gauche = "grand";
-            //alert("200px");
-
         }
         countmenuicon++;
-        $("#test_left").val($("main").css("left") + "nb de clic" +countmenuicon + "taille_menu" + taille_menu_gauche);
     });
 
 }
 
 
-//fonction appliquee une fois au chargement de la page
-// pour savoir si les menu doivent être visibles
-/*function etat_menu() {
-    if($(window).width() <= 768) {
-        $('main,#navbarleft').addClass('menu_gauche_masque')
-        $('main,#navbarright').addClass('menu_droit_masque')
-        //test
-        $("#test_classe").val($("#navbarleft").attr("class"));   
-    } else {
-        $("main, #navbarleft").addClass('menu_gauche_visible'); 
-        $("main, #navbarright").addClass('menu_droit_visible');
-        //test
-        $("#test_classe").val($("#navbarleft").attr("class"));    
-    }
-}*/
-
-
- 
 //fonction appliquee a chaque changement de la taille de la fenetre(.resize)
 //pour rendre visible ou masque les menu droit et gauche
 function taille_fenetre() {
@@ -127,7 +96,6 @@ function taille_fenetre() {
        
 
         if($(window).width() <= 768) {
-            //alert("frontière atteinte");
             if(action_utilisateur_gauche == "l'utilisateur a affiche le menu" ){
                 $("#navbarleft").css("left", "0px");
                 if (taille_menu_gauche == 'petit') {
@@ -148,7 +116,6 @@ function taille_fenetre() {
             }
             
         }else{
-            //test condition action utilisateur menu gauche
             if(action_utilisateur_gauche != "l'utilisateur a masque le menu") {
                 $("#navbarleft").css("left", "0px");
                 if (taille_menu_gauche == 'petit') {
@@ -158,9 +125,7 @@ function taille_fenetre() {
                 }
             }
             
-            //test condition action utilisateur menu droit
             if (action_utilisateur_droit != "l'utilisateur a masque le menu") {
-                //alert("bobo");
                 $("#navbarright").css("right", "0px");
                 $("main").css("right","200px");
             }
@@ -173,15 +138,6 @@ function taille_fenetre() {
 $(document).ready(function() {
     animation_menus_principaux();
     taille_fenetre();
-    //etat_menu();
-    $("#test_classe_gauche").val($("#navbarleft").attr("class"));
-    $("#test_classe_droit").val($("#navbarright").attr("class"));
-    //$("#test_bouton_gauche").val(countleft);
-    //$("#test_bouton_droit").val(countright);
-    //$("#test_variable_gauche").val(action_utilisateur_gauche);
-    //$("#test_variable_droit").val(action_utilisateur_droit);
-    $("#test_left").val($("main").css("left") + "nb de clic" +countmenuicon + "taille_menu" + taille_menu_gauche);
-
 });
 
 
